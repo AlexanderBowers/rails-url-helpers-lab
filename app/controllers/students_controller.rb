47290484ba14270c8f5f0ba9d61b12ca2d6e11c1
AlexanderBowers <7319abowers@gmail.com>
2020-12-14 10:53:19 -0800

    finished lab

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,12 +1,23 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :update]
   
   def index
     @students = Student.all
   end
 
   def show
+    @student = Student.find(params[:id])
   end
+
+  def update
+    @student.toggle!(:active)
+    redirect_to @student
+  end
+
+  def activate_student
+   
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: [:index, :show, :update]
+  
+  get 'students', to: 'students#index'
+  get 'students/:id/activate_student', to: 'students#update', as:'activate_student'
+
+  # GET '/students/:id/activate'
+
 end
